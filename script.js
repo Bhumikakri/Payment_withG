42a2292faa5beefc5bb36ed5a5d088a8065b48f1
Bhumikakri <128302166+Bhumikakri@users.noreply.github.com>
2023-10-24 21:23:12 +0530

    payment

--- script.js
+++ script.js
@@ -1,79 +1,45 @@
-let mainIcon = document.querySelector("#gpay");
-let startBtn = document.querySelector(".startbtn");
-let mainDiv = document.querySelector(".main");
-mainDiv.style.display = "none";
-let input =document.querySelector("#name_num");
-let amount=document.querySelector("#amount");
-let paybtn = document.querySelector(".Pay_btn");
+let checkboxx = [...document.querySelectorAll('input')];
+let btnn = document.querySelector('button');
+let ordersID = document.querySelector('span');
+let foods = document.querySelector(".foodImg");
+let audio = document.querySelector("audio");
 
-
-
-
-
-startBtn.addEventListener("click", ()=>{
-    mainDiv.style.display = "block";
-    startBtn.style.display = "none";
-    mainIcon.style.display = "none";
-
-})
-
-paybtn.addEventListener("click",()=>{
-    if(input.value ==""){
-        alert("Enter your mobile number");
-    }
-    else if(amount.value == ""){
-        alert("Enter Amount");
-    }
-    let mypromise=new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            if(input.value.length==10){
-                resolve("payment successfull");
-            }            
-            else{
-                if(input.value==""){
-                    reject("Enter your password");
-                }
-                else if(input.value.length!=10){
-                    reject("Enter your correct name or number");
-                }
-                else if(amount.value.length!=2){
-                    reject("Payement failed");
-                }
-            }
-        }, 5000);
-
+btnn.addEventListener('click', (e) =>{
+    // console.log(e);
+    audio.play();
+    e.preventDefault();
+    let AllFood = [];
+    checkboxx.forEach((checkbox) => {
+        if(checkbox.checked){
+            AllFood.push(checkbox.value);
+        }
     });
-
-    let procces = document.querySelector(".lower_main");
-
-    mypromise
-    .then((data)=>{
-    procces.innerHTML=`<img src="./OIP" alt="" id="g_icon2">
-    <h1 class="Paidamount">₹${amount.value}</h1>
-    <h3 class="status">${data}</h3>
-    <p class = transaction_id>Transaction ID: <span class="id">${
-      Math.floor(Math.random() * 90000000) + "@ybpl"
-    }</span></p>
-    <h2>Paid to: <span class="name_person">${input.value}</span></h2>
-    <button class = "Pay_btnn">Re Payment</button>`
-    
-    
-   })
-   .catch((mssg)=>{
-    procces.innerHTML=`<h3 class="status">${mssg}</h3>
-    <button class = "Pay_btnn">Re try</button>`
-   })
-
-//    let last = document.getElementsByClassName("Pay_btnn");
-})
-
-// function lastbt(last){
-//     last.addEventListener("click", ()=>{
-//         startBtn.style.display = "block";
-//         mainIcon.style.display = "block";
-//     })
-// }
-// lastbt();
+    if(AllFood.length == 0){
+        alert("Please select atleast one food item to order");
+        return;
+    }
 
 
-
+    btnn.disabled = true;
+    let myPromise = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            resolve();
+        }, Math.floor(Math.random() * 6000) + 100);
+    });
+    myPromise.then(() => {
+        ordersID.innerText = Math.floor(Math.random() * 1000);
+        AllFood.forEach((orderedItem) => {
+            if(orderedItem == "burger"){
+                foods.src = "https://th.bing.com/th/id/R.021f0b1eb00791d7c3d7d53eaf7eba6c?rik=tXyhA5UuKkPdzw&riu=http%3a%2f%2f3.bp.blogspot.com%2f_HPXWOvg7RcM%2fTSAlV0uVAnI%2fAAAAAAAAACo%2fkdGktUbFux8%2fs1600%2fburger.jpg&ehk=ijL%2bbELtrSC%2fB9P4qS8H8sezgLmFQQLWEx0%2fJB803x4%3d&risl=&pid=ImgRaw&r=0";
+            }
+            else if(orderedItem == "fries"){
+                foods.src = "https://th.bing.com/th/id/OIP.XeT3Q7d34b2Ltdax5wK9_wHaHl?pid=ImgDet&rs=1";
+            }
+            else if(orderedItem == "drinks"){
+                foods.src = "https://th.bing.com/th/id/OIP.VGOo7UyTCRr7UvSzPC5KSwHaFi?pid=ImgDet&rs=1"
+            }
+            foods.style.display = "inline-block";
+            btnn.disabled = false;
+        });
+    });
+});
